c8107bc8fa138997439fa3f54965042c08ab3690
Magda Mieczynska <magda.mieczynska@gmail.com>
2017-04-28 14:26:02 +0200

    Zadanie 3 z dodanym div

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $.ajax({
     url: "http://localhost:3000/books"
+    // typ domyslnie ustawiony jest na get wiec nie trzeba tego pisac
   }).done(function(response) {
     response.forEach(function(el, i) {
       // tworzenie nowego diva
       var newEl = $('<div class="list-group-item">');
       newEl.text(el.author);
-
+      var div = $("<div>");
+      newEl.append(div);
       $('ul').append(newEl);
+
       // console.log(el.author);
     });
   });
